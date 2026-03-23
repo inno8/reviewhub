96d612c30d0fdf89cf985ac7f386fdd6d652bc5d
assignon <kevineasky@gmail.com>
2026-03-23 23:08:35 +0100

    Add missing AppShell component

---
+++ frontend/src/components/layout/AppShell.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from './Header.vue';
+import Sidebar from './Sidebar.vue';
+</script>
+
+<template>
+  <div class="min-h-screen bg-background text-on-surface">
+    <Header />
+    <Sidebar />
+    <main class="ml-64 pt-16 min-h-screen flex flex-col">
+      <slot />
+    </main>
+  </div>
+</template>
